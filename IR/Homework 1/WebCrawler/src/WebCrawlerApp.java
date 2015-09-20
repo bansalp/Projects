911c38d56d25b81f89df36ba6daa863f840309cf
Parth Bansal <Parth1791@Parth-Bansals-MacBook.local>
2015-09-20 19:14:14 -0400

    Implemented part of focused crawling

--- IR/Homework 1/WebCrawler/src/WebCrawlerApp.java
+++ IR/Homework 1/WebCrawler/src/WebCrawlerApp.java
@@ -1,27 +1,47 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 public class WebCrawlerApp 
 {
 	static LinkedList<LinkNode> frontier = new LinkedList<LinkNode>();
 	static LinkedList<LinkNode> seen = new LinkedList<LinkNode>();
+	
+	static String seedPage = "https://en.wikipedia.org/wiki/Hugh_of_Saint-Cher";
+	static String keyPhrase = "index";
+	
 	static String prefix = "https://en.wikipedia.org/wiki/";
 	static String mainPage = "https://en.wikipedia.org/wiki/Main_Page";
+	
+	static String fileName = "URLs.txt";
+	static String charSet = "UTF-8";
+	
 	static String colon = ":";
+	static String pound = "#";
+	
+	static String parseHREFNodes = "a[href]";
+	static String parseHREF = "href";
+	
 	static int threshold = 1000;
+	
+	static int seedDepth = 1;
 	static int maxDepth = 5;
 	
 	public static void main(String[] args) 
 	{
 		try 
 		{
-			crawl("https://en.wikipedia.org/wiki/Hugh_of_Saint-Cher", "");
+			crawl();
 		} 
 		catch (Exception e) 
 		{
@@ -29,9 +49,9 @@ public class WebCrawlerApp
 		}
 	}
 	
-	public static void crawl(String seed, String keyphrase) throws IOException, InterruptedException
+	public static void crawl() throws IOException, InterruptedException
 	{
-		LinkNode seedNode = new LinkNode(seed, 0);
+		LinkNode seedNode = new LinkNode(seedPage, seedDepth);
 		frontier.add(seedNode);
 		
 		while (!frontier.isEmpty())
@@ -53,7 +73,12 @@ public class WebCrawlerApp
 			}
 		}
 		
-		PrintWriter writer = new PrintWriter("URLs.txt", "UTF-8");		
+		WriteURLsToFile();
+	}
+	
+	public static void WriteURLsToFile() throws FileNotFoundException, UnsupportedEncodingException
+	{
+		PrintWriter writer = new PrintWriter(fileName, charSet);		
 		for (LinkNode link: seen)
 		{
 			writer.println(link.getLink());
@@ -66,9 +91,9 @@ public class WebCrawlerApp
 		if (frontier.size() <= threshold)
 		{
 			Document doc = Jsoup.connect(node.getLink()).get();
-			Elements links = doc.select("a[href]");
+			Elements links = doc.select(parseHREFNodes);
 			FilterLinks(links, node);
-			//Thread.sleep(1000);
+			Thread.sleep(1000);
 		}
 	}
 	
@@ -76,7 +101,12 @@ public class WebCrawlerApp
 	{
 		for (Element link: links)
 		{
-			String absUrl = link.absUrl("href");
+			String absUrl = link.absUrl(parseHREF);
+			
+			if (ContainsPound(absUrl))
+			{
+				absUrl = absUrl.split(pound)[0];
+			}
 			
 			if (IsEnglishPrefixLink(absUrl))
 			{
@@ -84,8 +114,7 @@ public class WebCrawlerApp
 				{
 					if (!IsMainPageLink(absUrl))
 					{
-						int newNodeDepth = node.getDepth() + 1;
-						LinkNode newNode = new LinkNode(absUrl, newNodeDepth);
+						LinkNode newNode = new LinkNode(absUrl, node.getDepth() + 1);
 						
 						if ((!frontier.contains(newNode)) && (!seen.contains(newNode)))
 						{
@@ -96,7 +125,41 @@ public class WebCrawlerApp
 			}
 		}
 	}
+
+	public static Boolean ContainsKeyPhrase(String link) throws IOException
+	{
+		Document doc = Jsoup.connect(link).get();
+		Elements elements = doc.getElementsContainingOwnText(keyPhrase);
+		
+		for (Element element: elements)
+		{
+			if (IsURL(element.ownText()))
+			{
+				continue;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
 	
+	public static Boolean IsURL(String ownText)
+	{
+		try
+		{
+			URL u = new URL(ownText);
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
 	public static Boolean IsEnglishPrefixLink(String link)
 	{
 		return link.toLowerCase().contains(prefix.toLowerCase()); 
@@ -105,6 +168,11 @@ public class WebCrawlerApp
 	public static Boolean ContainsColon(String link)
 	{
 		return link.substring(prefix.length()).contains(colon);
+	}
+	
+	public static Boolean ContainsPound(String link)
+	{
+		return link.contains(pound);
 	}
 	
 	public static Boolean IsMainPageLink(String link)
