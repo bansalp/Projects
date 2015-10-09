09363d16166955efa191fe841d8310d632472703
bansalp <bansalp@ccs.neu.edu>
2015-10-08 23:35:59 -0400

    Partial implementation

--- IR/Homework 2/PageRank/src/PageRankApp.java
+++ IR/Homework 2/PageRank/src/PageRankApp.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -11,55 +10,70 @@ import java.util.Map;
 public class PageRankApp {
 	static String inlinks_file = Paths.get(".").toAbsolutePath().normalize().toString() + "/src/inlinks_file.txt";
 
-	static HashSet<String> all_nodes = new HashSet<String>();
+	static HashMap<String, Double> all_nodes = new HashMap<String, Double>();
 	static HashSet<String> sink_nodes = new HashSet<String>();
 
 	static HashMap<String, HashSet<String>> mapping = new HashMap<String, HashSet<String>>();
 	static HashMap<String, Integer> outlinks = new HashMap<String, Integer>();
 
 	static String whitespace = " ";
+	
+	static double prev_perplexity = 0.0;
+	static double cur_perplexity = 0.0;
+	static double damping_factor = 0.85;
 
 	public static void main(String[] args) {
-		try {
-			PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+		try 
+		{
+			loadFileInMemory();
 			
-			BufferedReader br = new BufferedReader(new FileReader(inlinks_file));
-			String str;
-
-			while ((str = br.readLine()) != null) {
-				String[] nodes = str.split(whitespace);
-				String newKey = nodes[0];
-				HashSet<String> temp;
-
-				if (!mapping.containsKey(newKey)) {
-					temp = new HashSet<String>();
-				}
-				else
+			for (Map.Entry<String, Double> entry : all_nodes.entrySet())
+			{
+				String node = entry.getKey();
+				if (!outlinks.containsKey(node))
 				{
-					temp = mapping.get(newKey);
-				}
-
-				all_nodes.add(newKey);
-
-				for (int i = 1; i < nodes.length; i++) {
-					if ((!temp.contains(nodes[i])) && (!(nodes[i].equalsIgnoreCase(newKey)))) {
-						temp.add(nodes[i]);
-
-						if (!outlinks.containsKey(nodes[i])) {
-							outlinks.put(nodes[i], 1);
-						} else {
-							outlinks.put(nodes[i], outlinks.get(nodes[i]) + 1);
-						}
-
-						all_nodes.add(nodes[i]);
-					}
-				}
-
-				if (temp.size() != 0)
-				{
-					mapping.put(newKey, temp);
+					sink_nodes.add(node);
 				}
 			}
+			
+			initializeValues();
+			
+			do 
+			{
+				double sinkPR = totalSinkPR();
+				
+				for (Map.Entry<String, Double> entry : all_nodes.entrySet())
+				{
+					double newPR = (1.0 - damping_factor) / all_nodes.size();
+					newPR += damping_factor * sinkPR / all_nodes.size();
+					
+					HashSet<String> temp = mapping.get(entry.getKey());
+					
+					if (temp != null)
+					{
+						Iterator<String> iterator = temp.iterator();
+						
+						while (iterator.hasNext())
+						{
+							String node = iterator.next();
+							newPR += damping_factor * all_nodes.get(node) / outlinks.get(node);
+						}
+					}
+					
+					entry.setValue(newPR);
+				}
+				
+				for (Map.Entry<String, Double> entry : all_nodes.entrySet())
+				{
+					System.out.println(entry.getKey() + ":" + entry.getValue() + " ");
+				}
+				
+				System.out.println();
+			}
+			while (hasConverged());
+			
+			
+			/*PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
 
 			System.out.println("Set of pages that link to page p");
 			writer.println("Set of pages that link to page p");
@@ -83,17 +97,17 @@ public class PageRankApp {
 
 			System.out.println("All Nodes");
 			writer.println("All Nodes");
-			Iterator<String> iterator = all_nodes.iterator();
-			while (iterator.hasNext()) {
-				String node = iterator.next();
-				System.out.println(node);
+			//Iterator<String> iterator = all_nodes.iterator();
+			for (Map.Entry<String, Double> entry : all_nodes.entrySet()) {
+				String node = entry.getKey();
+				System.out.println(node + " " + entry.getValue());
 				writer.println(node);
 			}
 			
-			iterator = all_nodes.iterator();
-			while (iterator.hasNext())
+			//iterator = all_nodes.iterator();
+			for (Map.Entry<String, Double> entry : all_nodes.entrySet())
 			{
-				String node = iterator.next();
+				String node = entry.getKey();
 				if (!outlinks.containsKey(node))
 				{
 					sink_nodes.add(node);
@@ -105,7 +119,7 @@ public class PageRankApp {
 			
 			System.out.println("Sink Nodes");
 			writer.println("Sink Nodes");
-			iterator = sink_nodes.iterator();
+			Iterator<String> iterator = sink_nodes.iterator();
 			while (iterator.hasNext())
 			{
 				String node = iterator.next();
@@ -113,9 +127,98 @@ public class PageRankApp {
 				writer.println(node);
 			}
 			
-			writer.close();
-		} catch (IOException e) {
+			writer.close();*/
+		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
 		}
+	}
+	
+	public static void loadFileInMemory() throws IOException
+	{
+		BufferedReader br = new BufferedReader(new FileReader(inlinks_file));
+		String str;
+		
+		while ((str = br.readLine()) != null) {
+			String[] nodes = str.split(whitespace);
+			String newKey = nodes[0];
+			HashSet<String> temp;
+
+			if (!mapping.containsKey(newKey)) {
+				temp = new HashSet<String>();
+			}
+			else
+			{
+				temp = mapping.get(newKey);
+			}
+
+			all_nodes.put(newKey, null);
+
+			for (int i = 1; i < nodes.length; i++) {
+				if ((!temp.contains(nodes[i])) && (!(nodes[i].equalsIgnoreCase(newKey)))) {
+					temp.add(nodes[i]);
+
+					if (!outlinks.containsKey(nodes[i])) {
+						outlinks.put(nodes[i], 1);
+					} else {
+						outlinks.put(nodes[i], outlinks.get(nodes[i]) + 1);
+					}
+
+					all_nodes.put(nodes[i], null);
+				}
+			}
+
+			if (temp.size() != 0)
+			{
+				mapping.put(newKey, temp);
+			}
+		}
+	}
+	
+	public static void initializeValues()
+	{
+		double val = 1.0/all_nodes.size();
+
+		for (Map.Entry<String, Double> entry : all_nodes.entrySet())
+		{
+			entry.setValue(val);
+		}
+	}
+	
+	public static double totalSinkPR()
+	{
+		double total = 0.0;
+		
+		for (String node: sink_nodes)
+		{
+			double val = all_nodes.get(node);
+			total += val;
+		}
+		
+		return total;
+	}
+	
+	public static double perplexity()
+	{
+		double total = 0.0;
+		
+		for (Map.Entry<String, Double> entry : all_nodes.entrySet())
+		{
+			double val = entry.getValue();
+			double log = Math.log(val) / Math.log(2);
+			double intermediateVal = val * log;
+			total += intermediateVal;
+		}
+		
+		total *= -1;
+		
+		return Math.pow(2, total);
+	}
+	
+	public static boolean hasConverged()
+	{
+		prev_perplexity = cur_perplexity;
+		cur_perplexity = perplexity();
+		
+		return (Math.abs(prev_perplexity - cur_perplexity) < 1.0 ? false : true);
 	}
 }
