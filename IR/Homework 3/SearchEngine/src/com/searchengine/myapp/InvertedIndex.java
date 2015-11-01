b10d175c133da16572622bb36d453d3ec4787472
bansalp <bansalp@ccs.neu.edu>
2015-11-01 17:49:57 -0500

    Added utility class implementing encapsulation

---
+++ IR/Homework 3/SearchEngine/src/com/searchengine/myapp/InvertedIndex.java
@@ -0,0 +1,111 @@
+package com.searchengine.myapp;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class InvertedIndex {
+	private String inputFileName;
+	private String outputFileName;
+	private String hashPattern;
+	private String digitsPattern;
+	private String spaceDelimeter;
+	private Map<String, Map<Integer, Long>> invertedIndex;
+	private Map<Integer, Long> termFrequency;
+	private Map<Integer, Long> numberOfTokens;
+
+	public InvertedIndex() {
+		hashPattern = "#.*";
+		digitsPattern = "[0-9]+";
+		spaceDelimeter = " ";
+
+		invertedIndex = new TreeMap<String, Map<Integer, Long>>();
+		numberOfTokens = new TreeMap<Integer, Long>();
+	}
+
+	public String getInputFileName() {
+		return inputFileName;
+	}
+
+	public void setInputFileName(String inputFileName) {
+		this.inputFileName = inputFileName;
+	}
+
+	public String getOutputFileName() {
+		return outputFileName;
+	}
+
+	public void setOutputFileName(String outputFileName) {
+		this.outputFileName = outputFileName;
+	}
+
+	public void create() {
+		try {
+			FileReader fr = new FileReader(inputFileName);
+			BufferedReader br = new BufferedReader(fr);
+			String line = null;
+			int docId = 0;
+			long counter = 0;
+
+			while ((line = br.readLine()) != null) {
+				if (line.matches(hashPattern)) {
+					docId = Integer.parseInt(line.split(spaceDelimeter)[1]);
+					counter = 0;
+				} else {
+					String[] words = line.split(spaceDelimeter);
+
+					for (String word : words) {
+						if (word.matches(digitsPattern)) {
+							continue;
+						} else {
+							counter++;
+
+							if (invertedIndex.containsKey(word)) {
+								termFrequency = invertedIndex.get(word);
+
+								if (termFrequency.containsKey(docId)) {
+									termFrequency.put(docId, termFrequency.get(docId) + 1);
+								} else {
+									termFrequency.put(docId, (long) 1);
+								}
+							} else {
+								termFrequency = new TreeMap<Integer, Long>();
+								termFrequency.put(docId, (long) 1);
+								invertedIndex.put(word, termFrequency);
+							}
+						}
+
+						numberOfTokens.put(docId, counter);
+					}
+				}
+			}
+
+			PrintWriter writer = new PrintWriter(outputFileName, "UTF-8");
+
+			writer.println("# Inverted Index: ");
+			for (Map.Entry<String, Map<Integer, Long>> entry : invertedIndex.entrySet()) {
+				writer.print(entry.getKey());
+				writer.print(":");
+
+				for (Map.Entry<Integer, Long> tf : invertedIndex.get(entry.getKey()).entrySet()) {
+					writer.print("(" + tf.getKey() + "," + tf.getValue() + ") ");
+				}
+
+				writer.println();
+			}
+
+			writer.println();
+
+			writer.println("# Number of Tokens: ");
+			for (Map.Entry<Integer, Long> entry : numberOfTokens.entrySet()) {
+				writer.println("(" + entry.getKey() + "," + entry.getValue() + ")");
+			}
+
+			writer.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
--- IR/Homework 3/SearchEngine/src/com/searchengine/myapp/SearchEngineApp.java
+++ IR/Homework 3/SearchEngine/src/com/searchengine/myapp/SearchEngineApp.java
@@ -1,109 +1,23 @@
 package com.searchengine.myapp;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.PrintWriter;
-import java.util.Map;
-import java.util.TreeMap;
+public final class SearchEngineApp {
+	private final static int TWO = 2;
 
-public class SearchEngineApp 
-{
-	private static String inputFileName = "src/tccorpus/tccorpus.txt";
-	//private static String inputFileName = "src/tccorpus/sample.txt";
-	private static String outputFileName = "src/tccorpus/index.out";
-	private static String hashPattern = "#.*";
-	private static String digitsPattern = "[0-9]+";
-	private static String spaceDelimeter = " ";
-	private static Map<String, Map<Integer, Long>> invertedIndex = new TreeMap<String, Map<Integer, Long>>();
-	private static Map<Integer, Long> termFrequency;
-	private static Map<Integer, Long> numberOfTokens = new TreeMap<Integer, Long>();
-	
-	public static void main (String[] args)
-	{
-		try 
-		{
-			FileReader fr = new FileReader(inputFileName);
-			BufferedReader br = new BufferedReader(fr);
-			String line = null;
-			int docId = 0;
-			long counter = 0;
-			
-			while ((line = br.readLine()) != null)
-			{
-				if (line.matches(hashPattern))
-				{
-					docId = Integer.parseInt(line.split(spaceDelimeter)[1]);
-					counter = 0;
-				}
-				else
-				{
-					String[] words = line.split(spaceDelimeter);
-					
-					for (String word: words)
-					{
-						if (word.matches(digitsPattern))
-						{
-							continue;
-						}
-						else
-						{
-							counter++;
-							
-							if (invertedIndex.containsKey(word))
-							{
-								termFrequency = invertedIndex.get(word);
-								
-								if (termFrequency.containsKey(docId))
-								{
-									termFrequency.put(docId, termFrequency.get(docId) + 1);
-								}
-								else
-								{
-									termFrequency.put(docId, (long) 1);
-								}
-							}
-							else
-							{
-								termFrequency = new TreeMap<Integer, Long>();
-								termFrequency.put(docId, (long) 1);
-								invertedIndex.put(word, termFrequency);
-							}
-						}
-						
-						numberOfTokens.put(docId, counter);
-					}
-				}
+	private SearchEngineApp() {
+	}
+
+	public static void main(final String[] args) {
+		if (args.length == TWO) {
+			try {
+				final InvertedIndex invertedIndex = new InvertedIndex();
+				invertedIndex.setInputFileName(args[0]);
+				invertedIndex.setOutputFileName(args[1]);
+				invertedIndex.create();
+			} catch (Exception e) {
+				System.out.println("Error: " + e.getMessage());
 			}
-			
-			PrintWriter writer = new PrintWriter(outputFileName, "UTF-8");
-			
-			writer.println("# Inverted Index: ");
-			for (Map.Entry<String, Map<Integer, Long>> entry: invertedIndex.entrySet())
-			{
-				writer.print(entry.getKey());
-				writer.print(":");
-				
-				for (Map.Entry<Integer, Long> tf: invertedIndex.get(entry.getKey()).entrySet())
-				{
-					writer.print("(" + tf.getKey() + "," + tf.getValue() + ") ");
-				}
-				
-				writer.println();
-			}
-			
-			writer.println();
-			
-			writer.println("# Number of Tokens: ");
-			for (Map.Entry<Integer, Long> entry: numberOfTokens.entrySet())
-			{
-				writer.println("(" + entry.getKey() + "," + entry.getValue() + ")");
-			}
-			
-			writer.close();
-		}
-		catch (Exception e) 
-		{
-			System.out.println("Error: " + e.getMessage());
+		} else {
+			System.out.println("Illegal number of arguments !");
 		}
 	}
 }
