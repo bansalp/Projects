ad21b57253f201547d23b8eb5b28055747666ce0
bansalp <bansalp@ccs.neu.edu>
2015-11-28 20:30:45 -0500

    Changed review to ClassAttribute

--- IR/Homework 6/TextClassification/src/com/textclassification/myapp/GenerateARFF.java
+++ IR/Homework 6/TextClassification/src/com/textclassification/myapp/GenerateARFF.java
@@ -3,38 +3,43 @@ package com.textclassification.myapp;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
 
 public class GenerateARFF 
 {
 	private FastVector atts;
+	private FastVector mr;
 	private Instances data;
-	private double[] vals;
 
 	public GenerateARFF() 
 	{
 		// 1. set up attributes
 		atts = new FastVector();
+		
+		mr = new FastVector(2);
+		mr.addElement("positive");
+		mr.addElement("negative");
+		Attribute mrAttr = new Attribute("mr", mr);
 
 		// - string
 		atts.addElement(new Attribute("text", (FastVector) null));
-		atts.addElement(new Attribute("review", (FastVector) null));
+		atts.addElement(new Attribute("review", mr));
 
 		// 2. create Instances object
 		data = new Instances("MovieReviews", atts, 0);
+		data.setClassIndex(data.numAttributes() - 1);
 	}
 
 	public void addInstance(String text, String review) 
 	{
 		// 3. fill with data
-		vals = new double[data.numAttributes()];
-		
-		// - string
-		vals[0] = data.attribute(0).addStringValue(text);
-		vals[1] = data.attribute(1).addStringValue(review);
+		Instance inst = new DenseInstance(2);
+		inst.setValue((Attribute)atts.elementAt(0), text);
+		inst.setValue((Attribute)atts.elementAt(1), review);
 
 		// add
-		data.add(new DenseInstance(1.0, vals));
+		data.add(inst);
 	}
 	
 	public Instances getInstances()
